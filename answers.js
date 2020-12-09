e67b694821ba8b58c907d588a17264afc9b7f998
Forspand <71700626+Forspand@users.noreply.github.com>
2020-12-09 15:11:44 +0100

    Update answers.js

--- answers.js
+++ answers.js
@@ -1,33 +1,33 @@
 // words
-var yesWords = ["Ja", "Yeps", "Helt klart", "Jepper", "Tjek", "True", "Yeah", "Jep", "Yes"];
-var noWords = ["Nej", "Næh", "Nope", "Niksen biksen", "Næppe", "Helt klart ikke", "No", "Nah", "False"];
-var catAnswers = ["Ja", "Nej"];
+// var yesWords = ["Ja", "Yeps", "Helt klart", "Jepper", "Tjek", "True", "Yeah", "Jep", "Yes"];
+// var noWords = ["Nej", "Næh", "Nope", "Niksen biksen", "Næppe", "Helt klart ikke", "No", "Nah", "False"];
+// var catAnswers = ["Ja", "Nej"];
 
 // chose the words
-var yesWord1 = yesWords[Math.floor(Math.random() * yesWords.length)];
-var yesWord2 = yesWords[Math.floor(Math.random() * yesWords.length)];
-var noWord = noWords[Math.floor(Math.random() * noWords.length)];
-var catAnswer = catAnswers[Math.floor(Math.random() * catAnswers.length)];
+// var yesWord1 = yesWords[Math.floor(Math.random() * yesWords.length)];
+// var yesWord2 = yesWords[Math.floor(Math.random() * yesWords.length)];
+// var noWord = noWords[Math.floor(Math.random() * noWords.length)];
+// var catAnswer = catAnswers[Math.floor(Math.random() * catAnswers.length)];
 
 // get the current date
-var date = new Date();
-var dato = date.getDay();
-var time = date.getHours()*100 + date.getMinutes();
+// var date = new Date();
+// var dato = date.getDay();
+// var time = date.getHours()*100 + date.getMinutes();
 
 // special days (remember, january is month 0)
-var cleanDay = new Date(2017,10,8);
-var cleanDiff = date - cleanDay;
-var cleanDays = Math.floor(cleanDiff/(60*60*1000*24)*1);
+// var cleanDay = new Date(2017,10,8);
+// var cleanDiff = date - cleanDay;
+// var cleanDays = Math.floor(cleanDiff/(60*60*1000*24)*1);
 
 // extra phrase (should at least be ".")
-var extra = ", så længe du spritter af, bærer mundbind når du bevæger dig rundt, og ikke laver mad i køkkenet."
+// var extra = ", så længe du spritter af, bærer mundbind når du bevæger dig rundt, og ikke laver mad i køkkenet."
 
 // say if Nanobar is open
-if (dato == 0 || dato == 6) {document.getElementById("svar1").innerHTML = yesWord1 
-	+ extra + "<br><small>Bare husk studiekort i weekenden.</small>";}
-else if (1700 <= time || time <= 700) {document.getElementById("svar1").innerHTML = yesWord1 
-	+ extra + "<br><small>Husk studiekort efter kl. 17.</small>";}
-else {document.getElementById("svar1").innerHTML = yesWord1 + extra;}
+// if (dato == 0 || dato == 6) {document.getElementById("svar1").innerHTML = yesWord1 
+//	+ extra + "<br><small>Bare husk studiekort i weekenden.</small>";}
+// else if (1700 <= time || time <= 700) {document.getElementById("svar1").innerHTML = yesWord1 
+//	+ extra + "<br><small>Husk studiekort efter kl. 17.</small>";}
+// else {document.getElementById("svar1").innerHTML = yesWord1 + extra;}
 
 // tell days since cleaning
 // document.getElementById("q2").innerHTML = "Dage siden sidste hovedrengøring: " + cleanDays;
@@ -35,4 +35,6 @@ else {document.getElementById("svar1").innerHTML = yesWord1 + extra;}
 // solve quantum mechanics
 document.getElementById("svar3").innerHTML = catAnswer + ".";
 
+// Pandemic message - please figure out how to say this
+{document.getElementById("svar1").innerHTML = "Nej, Nanobar er lukket pga. COVID-19. Vi melder ud, når der genåbnes og håber på dette kan ske tidligt i det nye år (2021)."
 
